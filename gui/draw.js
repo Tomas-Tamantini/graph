b828ba1daf33d5227b4b7b8912635b785618e0f2
Tomas Tamantini <tomasbft@gmail.com>
2020-10-03 09:19:03 -0300

    Add mouse handler

--- gui/draw.js
+++ gui/draw.js
@@ -1,18 +1,26 @@
 const DRAW_STYLE = {
-  DEFAULT: 0,
+  DEFAULT: 1,
+  MOUSE_HOVER: 2,
+  BEING_DRAGGED: 3,
 }
 
 function draw_node(node, style = DRAW_STYLE.DEFAULT) {
   push()
   translate(node.position.x, node.position.y)
-  let radius
+  let radius = 18
   switch (style) {
+    case DRAW_STYLE.MOUSE_HOVER:
+      radius = 20
+      break
+    case DRAW_STYLE.BEING_DRAGGED:
+      fill(0, 255, 0)
+      break
     default:
-      radius = 18
       break
   }
   ellipse(0, 0, radius)
 
+  fill(0)
   let lbl = node.label + ""
   let off_y = radius / 4.5
   let off_x = lbl.length == 1 ? -radius / 5.2 : -radius / 2.6
@@ -32,7 +40,20 @@ function draw_edge(edge, style = DRAW_STYLE.DEFAULT) {
   pop()
 }
 
-function draw_graph(graph) {
+function draw_graph(graph, mouse_handler) {
+  mouse_handler.update_node_position()
   for (let edge of graph.edges) draw_edge(edge)
-  for (let node of graph.nodes) draw_node(node)
+  for (let node of graph.nodes) {
+    let style = DRAW_STYLE.DEFAULT
+    if (mouse_handler.node_being_dragged) {
+      if (node === mouse_handler.node_being_dragged) {
+        style = DRAW_STYLE.BEING_DRAGGED
+      }
+    } else {
+      if (node === mouse_handler.node_with_mouse_over()) {
+        style = DRAW_STYLE.MOUSE_HOVER
+      }
+    }
+    draw_node(node, style)
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
 function setup() {
-  cnv = createCanvas(windowWidth / 2, windowHeight / 2)
-  cnv.doubleClicked(mouseDoubleClicked)
-  g = adjacent_primes(5)
+  canvas = createCanvas(windowWidth / 2, windowHeight / 2)
+  canvas.doubleClicked(mouseDoubleClicked)
+
+  graph = adjacent_primes(5)
+  mouse_handler = new MouseHandler(graph)
 }
 
 function draw() {
   background(51, 56, 66)
-  draw_graph(g)
+  draw_graph(graph, mouse_handler)
 }
 
 function mousePressed() {
-  console.log("mouse pressed", mouseX, mouseY)
+  mouse_handler.mouse_pressed()
 }
 
 function mouseReleased() {
-  console.log("mouse released", mouseX, mouseY)
+  mouse_handler.mouse_released()
 }
 
 function mouseDoubleClicked() {
